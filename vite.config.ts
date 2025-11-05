4f4ecc762026ca8506f37c0efcaddf5563058983
codingsh <codingsh@pm.me>
2025-11-05 08:32:47 +0000

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -22,5 +23,9 @@ export default defineConfig({
       },
     },
     chunkSizeWarningLimit: 1000,
+    
   },
+  server: {
+    historyApiFallback: true
+  }
 })
